35a5b1bcd99a015b42cf59ad96037958ea472d8e
max <belocoor@gmail.com>
2019-10-14 21:46:09 +0300

    f

---
+++ JS/goit-js-hw-01/JS/task2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const total = 100;
+let ordered = 50;
+
+if (ordered > total) {
+    console.log('На складе недостаточно твоаров!');
+}else if (ordered < total){
+    console.log('Заказ оформлен, с вами свяжется менеджер')
+}
+
+
+
+
